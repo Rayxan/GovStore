36d89ea602bfd8dc37f3079960755ad1cebc53ac
Raylan Sales <Raylanwork@gmail.com>
2022-07-05 21:07:33 -0300

    Iniciando o front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,27 @@
 
 @section('content')
 
-@foreach($aplicativos as $aplicativo)
-    <p>{{ $aplicativo->nm_nome_aplicativo }} --
-     {{ $aplicativo->ds_descricao_aplicativo }}    
-    </p>
-@endforeach
+<div class="col-md-12" id="search-container">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Pesquisar...">
+    </form>
+</div>
+<div id="aplicativos-container" class="col-md-12">
+    <h2>Próximos Aplicativos</h2>
+    <p>Veja todos os aplicativos</p>
+    <div id="cards-container" class="row">
+        @foreach($aplicativos as $aplicativo)
+        <div class="card col-md-3">
+            <h5 class="card-name">{{ $aplicativo->nm_nome_aplicativo}}</h5>
+            <img src="/img/dragons.jpeg" alt="{{ $aplicativo->nm_nome_aplicativo}}">
+            <div class="card-body">
+                <p class="card-description">{{$aplicativo->ds_descricao_aplicativo}}</p>
+                <a href="#" class="btn btn-primary">Acessar</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
